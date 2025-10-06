54ae0c77930d54c0b73333108f07b474c0f2e815
RenanAlfenas <alfenas57@gmail.com>
2025-10-06 19:14:55 -0300

    exercicios

---
+++ JavaScript/Módulo1/teste8.js
@@ -0,0 +1,19 @@
+let user = [
+    'joão',
+    'carlos',
+    'andré',
+    'renan',
+    'andereia',
+    'aline',
+    'clarisse',
+    'renata',
+    'joana',
+    'inês',
+    'carlos',
+    
+]
+
+for (let i = 0; i < user.length; i++ ) {
+    console.log(user[i])
+}
+
