0d1276ce7a74b71f0236c99bdae5aa0033931d8a
RenanAlfenas <alfenas57@gmail.com>
2025-10-07 20:51:01 -0300

    teste

---
+++ JavaScript/Módulo1/desafio for/scrip.js
@@ -0,0 +1,18 @@
+/*
+
+carcular descontos:
+
+Todos os produtos a cima de R$ 30,00 tem desconto de 10%
+
+*/
+
+let cart = [10,30,40,5,80,6]
+
+
+for (let i = 0; i < cart.length; i++) {
+    const element = cart[i];
+    let som = soma + element
+
+    console.log(som)
+    
+}
