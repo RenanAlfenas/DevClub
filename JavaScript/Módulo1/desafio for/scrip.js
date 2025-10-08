d32cfd65f1d44c0a79cb516e08e6d4f73b14adde
RenanAlfenas <alfenas57@gmail.com>
2025-10-08 11:13:39 -0300

    exercicio de descontos

--- JavaScript/Módulo1/desafio for/scrip.js
+++ JavaScript/Módulo1/desafio for/scrip.js
@@ -6,13 +6,39 @@ Todos os produtos a cima de R$ 30,00 tem desconto de 10%
 
 */
 
-let cart = [10,30,40,5,80,6]
+let cart = [
+    { valor: 80 },
+    { valor: 20 },
+    { valor: 10 },
+   
+]
 
+let valorInicial = 0
 
-for (let i = 0; i < cart.length; i++) {
-    const element = cart[i];
-    let som = soma + element
-
-    console.log(som)
-    
+function MeuDesconto(preco,desconto) {     // essa função serve para calcular o desconto dos itens e retornar o valor a variável desc.
+    let desc = (preco*desconto)/100
+    return desc
 }
+
+cart.forEach(itens => {
+    
+    if (itens.valor > 30) {
+        valorInicial = valorInicial + (itens.valor - MeuDesconto(itens.valor,10))  // essa linha serve para somar os valores que tem desconto 
+    } else {
+        valorInicial += itens.valor   // essa linha serve para somar os valores que não tem  desconto e ai soma todos os valores do meu array
+    }
+
+});
+
+
+console.log(valorInicial)
+
+
+
+
+
+
+
+
+
+
