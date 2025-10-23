a6a40839e2f0229e8e114f38e7dc6bfa7b0166b1
RenanAlfenas <alfenas57@gmail.com>
2025-10-22 21:01:52 -0300

    finalização do execício de cardápio

--- JavaScript/Módulo1/ProjetoCardapio/assets/products.js
+++ JavaScript/Módulo1/ProjetoCardapio/assets/products.js
@@ -1,18 +1,21 @@
 const menuOptions = [
 
-{ name: 'X-Salada', price: 30.00, vegan: false, src: '../imagens/xsalada.jpeg' },
+{ name: 'X-Salada', price: 30, vegan: false, src: '../imagens/xsalada.jpeg' },
 
-{ name: 'X-Bacon', price: 34.00, vegan: false, src: '../imagens/xbacon.png' },
+{ name: 'X-Bacon', price: 34, vegan: false, src: '../imagens/xbacon.png' },
 
-{ name: 'X-Bacon Egg', price: 39.00, vegan: false, src: '../imagens/bacon-egg.png' },
+{ name: 'X-Bacon Egg', price: 39, vegan: false, src: '../imagens/bacon-egg.png' },
 
-{ name: 'Monstruoso', price: 50.00, vegan: false, src: '../imagens/monstruoso.png' },
+{ name: 'Monstruoso', price: 50, vegan: false, src: '../imagens/monstruoso.png' },
 
-{ name: 'Big Vegano', price: 55.00, vegan: true, src: '../imagens/xvegan.png' },
+{ name: 'Big Vegano', price: 55, vegan: true, src: '../imagens/xvegan.png' },
 
-{ name: 'X-Vegan', price: 45.00, vegan: true, src: '../imagens/monstruoso-vegan.png' },
+{ name: 'X-Vegan', price: 45, vegan: true, src: '../imagens/monstruoso-vegan.png' },
 
 ]
 
 
 let section = document.getElementById('products')
+
+
+
--- JavaScript/Módulo1/ProjetoCardapio/assets/scripts.js
+++ JavaScript/Módulo1/ProjetoCardapio/assets/scripts.js
@@ -1,5 +1,7 @@
 function MostrarTudo() {
-    if (section.children.length === 0) {
+
+        section.innerHTML = ""
+
 
         menuOptions.forEach(item => {
 
@@ -17,29 +19,99 @@ function MostrarTudo() {
 
         let p = document.createElement('p')
         p.classList.add('element')
-        p.innerHTML = item.price
+        p.innerHTML = `R$ ${item.price.toFixed(2).replace('.',',')}`
         div.appendChild(p);
 
         
       
-   
-    });
-} else {
-    alert('já está com todos os itens a mostra')
+})
 }
-}
+
 
 
 function desconto() {
-            menuOptions.map ((NewPrice,index) => {
+        section.innerHTML = ""
 
-               const novoPreco = NewPrice.price - (NewPrice.price/10)
+                menuOptions.map ((NewPrice) => {
 
-                    let p = document.querySelectorAll('.element')
+                    let novoPreco = NewPrice.price - (NewPrice.price/10)
 
-                    p[index].textContent = novoPreco;
+                    let div = document.createElement('div')
+                        div.id = `${NewPrice.name}`
+                        section.appendChild(div);
+                
+                    let img = document.createElement('img')
+                        img.src = `${NewPrice.src}`
+                        div.appendChild(img);
 
-        } 
+                    let h1 = document.createElement('h1')
+                        h1.innerText = NewPrice.name
+                        div.appendChild(h1);
+
+                    let p = document.createElement('p')
+                        p.classList.add('element')
+                        p.innerHTML = `R$ ${novoPreco.toFixed(2).replace('.',',')}`
+                        div.appendChild(p);
+
+                } 
 ) 
+
+}
+
+function sum() {
+
+    let soma = menuOptions.reduce((acum,item) => {
+        return acum + item.price
+    },0)
+
+    
+    section.innerHTML = ""
+    
+    let div = document.createElement('div')
+        div.id = 'texto'
+        section.appendChild(div);
+    
+    let h1 = document.createElement('h1')
+        h1.innerHTML = 'o valor do cardápio total é: '
+        div.appendChild(h1);
+
+    let p = document.createElement('p')
+        p.innerHTML = `R$ ${soma.toFixed(2).replace('.',',')}`
+        div.appendChild(p)   
+}
+
+
+function filter() {
+    let filtro = menuOptions.filter( validation => {
+        if (validation.vegan == true) {
+            return true
+        } else {
+            return false
+        }
+    })
+
+    
+    section.innerHTML = ""
+
+    for (let i = 0; i < filtro.length; i++) {
+        
+        let div = document.createElement('div')
+        div.id = `${filtro[i].name}`
+        section.appendChild(div);
+        
+        let img = document.createElement('img')
+        img.src = `${filtro[i].src}`
+        div.appendChild(img);
+
+        let h1 = document.createElement('h1')
+        h1.innerText = filtro[i].name
+        div.appendChild(h1);
+
+        let p = document.createElement('p')
+        p.classList.add('element')
+        p.innerHTML = `R$ ${filtro[i].price.toFixed(2).replace('.',',')}`
+        div.appendChild(p);
+        
+    }
 
 }
