c1727c6a95e28443ecf3288cf6a7192403c3e706
RenanAlfenas <alfenas57@gmail.com>
2025-10-28 12:02:56 -0300

    finalização de todolist

--- JavaScript/ProjetosPessoais/ToDoList/assets/script.js
+++ JavaScript/ProjetosPessoais/ToDoList/assets/script.js
@@ -9,7 +9,7 @@ let completButton = document.getElementsByClassName('complet')
 let ul = document.getElementById('tasks')
 
 
-function addTaskButton() {
+function addTaskButton() {               // essa função serve para adicionar as tarefas na minha lista de tarefas
     if(inputText.value != "") {
     
     list.innerHTML += `
@@ -19,7 +19,7 @@ function addTaskButton() {
         
                 <span>
                     <button class="delet" onclick = "deletButton(this)">Excluir</button>
-                    <button class="edit">Editar</button>
+                    <button class="edit" onclick = "editButton(this)">Editar</button>
                     <button class="completButton" onclick = "CompletTask(this)">Concluido</button>
                 </span>
         
@@ -30,23 +30,32 @@ function addTaskButton() {
     }
 }
 
-inputText.addEventListener('keydown' , function(e) {
+inputText.addEventListener('keydown' , function(e) {               // essa função serve para poder adicionar a tarefa apertando ENTER ao invés de só usar o botão
     if(e.key === 'Enter') {
         addTaskButton()
     }
 })
 
-function CompletTask(button) {
-    let p = button.closest('li').querySelector('p')
+function CompletTask(button) {                                    // essa função serve para marcar a função como completa deixando ela cinza e com um risco
+    let p = button.closest('li').querySelector('p')               // essa linha seleciona o <p> mais perto(closest) da minha <li> que no caso é o próprio <p> da <li>
     p.classList.toggle('complet')
     
 }
 
-function deletButton(button) {
-    let li = button.closest('li')
+function deletButton(button) {                                   // essa função serve para deletar a a tarefa deletando toda a <li>
+    let li = button.closest('li')                               // essa linha pega o <li> mais perto do meu button que é o <li> onde está o button
     li.remove()
 }
 
+
+function editButton(button) {                                   // essa função serve para editar o valor do meu <p> usando o prompt de comando
+   let editText = prompt('Edite o Texto')
+    if(editText != null) {               // essa condição serve para checar se o valor do prompt está mesmo preenchido, caso não esteja, ele deixa o texto do jeito que está
+    let p = button.closest('li').querySelector('p')
+    p.innerHTML = editText
+    }
+
+}
 
 
 
