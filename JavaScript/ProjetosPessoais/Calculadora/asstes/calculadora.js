e6f6b8b5624337598fdded57eaf850aa9fc28c8e
RenanAlfenas <alfenas57@gmail.com>
2025-10-29 16:00:04 -0300

    projeto calculadora

--- JavaScript/ProjetosPessoais/Calculadora/asstes/calculadora.js
+++ JavaScript/ProjetosPessoais/Calculadora/asstes/calculadora.js
@@ -7,6 +7,18 @@ function insert(n) {
 
 }
 
+function percent() {
+    let n1 = calc.value/100
+    calc.value = n1
+    
+}
+
+function squad() {
+    let n1 = calc.value.replace(/[()]/g,'')
+    let n2 = Math.sqrt(n1,2)
+    calc.value = n2
+}
+
 function resultExpression() {
     console.log(calc.value)
     result.value = ""
@@ -21,3 +33,4 @@ function apagar() {
 function delet() {
     calc.value = calc.value.slice(0,-1)
 }
+
