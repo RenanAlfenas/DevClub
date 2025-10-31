57b8ebc5c4f454c63dff11d2cce4f8c83d03e3b1
RenanAlfenas <alfenas57@gmail.com>
2025-10-31 17:44:22 -0300

    uso de banco de dados

--- Node/server.js
+++ Node/server.js
@@ -1,15 +1,30 @@
 import express from 'express'
-
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
 const app = express()
 app.use(express.json())
 
-app.get('/usuarios/', (req,res) => {
-    res.send('olá! você consegui acessar o servidor com sucesso!')
+app.get('/usuarios/', async (req,res) => {
+
+        const users = await prisma.user.findMany()
+    res.status(200).json(`${users[4].name} sua idade é ${users[4].age} anos`)
+  
+
+    console.log(users)
 })
 
-app.post('/usuarios', (req,res) => {
-    res.send('ola mano!')
-    console.log(req)
+app.post('/usuarios', async (req,res) => {
+   const person =  await prisma.user.create({
+        data: {
+            name: req.body.name,
+            age: req.body.age,
+            email: req.body.email
+        }
+    })
+    res.status(201).json({mensage: "usuário cadastrado com sucesso"})
+    console.log(person)
 })
+
+
 
 app.listen(3000)
