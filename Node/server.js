024cf189fee7ffe4c8c55ee5b3b56d491b475704
RenanAlfenas <alfenas57@gmail.com>
2025-10-31 20:53:31 -0300

    delet

--- Node/server.js
+++ Node/server.js
@@ -4,10 +4,10 @@ const prisma = new PrismaClient()
 const app = express()
 app.use(express.json())
 
-app.get('/usuarios/', async (req,res) => {
+app.get('/usuarios', async (req,res) => {
 
         const users = await prisma.user.findMany()
-    res.status(200).json(`${users[4].name} sua idade é ${users[4].age} anos`)
+    res.status(200).json(users)
   
 
     console.log(users)
@@ -25,6 +25,31 @@ app.post('/usuarios', async (req,res) => {
     console.log(person)
 })
 
+app.put('/usuarios/:id', async (req,res) => {
+   const person =  await prisma.user.update({
+        where: {
+            id: req.params.id
+        },
+        data: {
+            name: req.body.name,
+            age: req.body.age,
+            email: req.body.email
+        }
+    })
+    res.status(201).json({mensage: "usuário cadastrado com sucesso"})
+    console.log(person)
+})
+
+
+app.delete('/usuarios/:id', async (req,res) => {
+    await prisma.user.delete ({
+        where:{
+            id:req.params.id,
+        } 
+    })
+
+    res.status(200).json({mensage: "Usuário deletado com sucesso!"})
+})
 
 
 app.listen(3000)
