12a0aeed36543a9430eed993be4847452f98fff8
RenanAlfenas <alfenas57@gmail.com>
2025-11-01 18:50:33 -0300

    estudos javascript avançado

---
+++ JavaScrip-Avançado/allFunctions.js
@@ -0,0 +1,9 @@
+function sum(n1,n2) {
+    return n1 + n2
+}
+
+function mult(n1,n2) {
+    return n1*n2
+}
+
+module.exports = {sum,mult}
---
+++ JavaScrip-Avançado/result.js
@@ -0,0 +1,4 @@
+const allObjects = require('./allFunctions')
+
+
+console.log(allObjects.mult(10,2))
